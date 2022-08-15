06d7408a59afc399aadd424579753866ccba54a3
Michal Gregorczyk <michalgr@live.com>
2022-08-15 15:58:31 +0200

    [xbows/knight:michalgr] add GRV in the DIGIS layers

--- keyboards/xbows/knight/keymaps/michalgr/keymap.c
+++ keyboards/xbows/knight/keymaps/michalgr/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO,          KC_NO,
     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_PSLS, KC_PAST, KC_PMNS, KC_NO,   KC_NO,       KC_NO,
     KC_NO,   KC_EXLM, KC_AT,   KC_PAST, KC_PSLS, KC_LT,                     KC_NO,   KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO,   KC_HASH, KC_DLR,  KC_PPLS, KC_PMNS, KC_NO,     KC_BSPC,        KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_PCMM, KC_NO,        KC_NO,       KC_NO,
+    KC_NO,   KC_HASH, KC_DLR,  KC_PPLS, KC_PMNS, KC_GRV,    KC_BSPC,        KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_PCMM, KC_NO,        KC_NO,       KC_NO,
     KC_NO,   KC_PERC, KC_CIRC, KC_PIPE, KC_BSLS, KC_GT,     KC_ENT,         KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_PEQL, KC_NO,            KC_NO,
     KC_NO,   KC_NO,       KC_NO,             KC_SPC,      KC_NO,   KC_NO,         KC_P0,           KC_PDOT,     KC_PENT, KC_NO,   KC_NO,   KC_NO,   KC_NO
   ),
